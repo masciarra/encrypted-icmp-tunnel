837f58e719eed726f40f7e8bc8024878818f921b
Max Sciarra <maxsciarra@Maxs-MacBook-Pro-SSD.local>
2020-08-07 18:34:29 -0700

    updated client, readme

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
+Encrypted ICMP Tunnel in Python
+
+Client and server script to send/receive encrypted text over ICMP
+
 References ICMP code from:
 
 https://github.com/Vidimensional/Icmp-File-Transfer
+
+Works with Python 2 and 3
+
+To run the client, run sudo python /client/client.py <txt file to send> <ip to send to>
+
+To run the server, run sudo python /server/server.py
+
+Server prints incoming decrypted data to terminal
--- client/client.py
+++ client/client.py
@@ -1,9 +1,10 @@
 import sys
 from subprocess import Popen, PIPE
 
-ip = sys.argv[1]
+ip = sys.argv[2]
+inputpath = sys.argv[1]
 
-process = Popen(['python', '../substitution_encryption.py', 'encrypt', 'handshake.txt','data.txt'])
+process = Popen(['python', '../substitution_encryption.py', 'encrypt', inputpath,'data.txt'])
 process.wait()
 process = Popen(['sudo', 'python', 'Icmp-File-Transfer/icmp.py', 'send', 'data.txt', ip])
 
